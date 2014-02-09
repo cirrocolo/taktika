73df20e3d7640d2f1d1d81ad5dc48a079c420757
cirrocolo <cirrocolo@outlook.com>
2014-02-09 21:42:30 +0100

    Spelidé färdig i förstudiedokumentet

--- src/Project L/Project1/Source.cpp
+++ src/Project L/Project1/Source.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <Windows.h>
 
-void test1()
+void test1() // simple console IO
 {
 	int number;
 
 	std::cout << "Type a number" << std::endl;
 	std::cin >> number;
 	std::cout << "The number you typed was: " << number << std::endl;
-	while (true); // this is here so the console output on windows will not close
-}
+	}
 
 int main()
 {
 	test1(); // simple console IO
 	
+	// while (true); // this is here so the console output on windows will not close
+	
+	Sleep(1000); // Windows
+
 	return 0;
 }
